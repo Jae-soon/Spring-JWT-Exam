eb9f7942310e45d1bb1341e4ec3bc32ba0d25b61
Jae-Soon Kwon <91954574+Jae-soon@users.noreply.github.com>
2022-10-06 14:44:38 +0900

    로그인 성공시 응답 본문에도 출력

--- src/main/java/com/ll/exam/jwt_login/app/member/controller/MemberController.java
+++ src/main/java/com/ll/exam/jwt_login/app/member/controller/MemberController.java
@@ -37,10 +37,16 @@ public class MemberController {
             return Util.spring.responseEntityOf(RsData.of("F-3", "비밀번호가 일치하지 않습니다."));
         }
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authentication", "JWT_Access_Token");
+        HttpHeaders headers = Util.spring.httpHeadersOf("Authentication", "JWT_Access_Token");
 
-        return Util.spring.responseEntityOf(RsData.of("S-1", "로그인 성공, Access Token을 발급합니다."), headers);
+        String accessToken = "JWT_Access_Token";
+
+        return Util.spring.responseEntityOf(
+                RsData.of("S-1", "로그인 성공, Access Token을 발급합니다.",
+                        Util.mapOf("accessToken", accessToken)
+                ),
+                Util.spring.httpHeadersOf("Authentication", "JWT_Access_Token")
+        );
     }
 
     @Data
