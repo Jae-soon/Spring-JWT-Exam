04e9f7f1322816bc54a97a086b920330fc4756b7
Jae-Soon Kwon <91954574+Jae-soon@users.noreply.github.com>
2022-10-13 15:22:31 +0900

    Feat : Cacheable

--- src/main/java/com/ll/exam/jwt_login/app/member/service/MemberService.java
+++ src/main/java/com/ll/exam/jwt_login/app/member/service/MemberService.java
@@ -4,6 +4,7 @@ import com.ll.exam.jwt_login.app.member.entity.Member;
 import com.ll.exam.jwt_login.app.member.repository.MemberRepository;
 import com.ll.exam.jwt_login.app.security.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
@@ -48,5 +49,11 @@ public class MemberService {
 
     public boolean verifyWithWhiteList(Member member, String token) {
         return member.getAccessToken().equals(token);
+    }
+
+    @Cacheable("key1")
+    public int getCachedInt() {
+        System.out.println("getCachedInt 호출됨");
+        return 5;
     }
 }
