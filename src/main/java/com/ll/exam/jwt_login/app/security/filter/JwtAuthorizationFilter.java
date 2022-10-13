126c8ffdb2d4db159ea241fe04f0eaec63185261
Jae-Soon Kwon <91954574+Jae-soon@users.noreply.github.com>
2022-10-13 14:28:11 +0900

    Use CPU to loading

--- src/main/java/com/ll/exam/jwt_login/app/security/filter/JwtAuthorizationFilter.java
+++ src/main/java/com/ll/exam/jwt_login/app/security/filter/JwtAuthorizationFilter.java
@@ -2,16 +2,13 @@ package com.ll.exam.jwt_login.app.security.filter;
 
 
 import com.ll.exam.jwt_login.app.member.entity.Member;
-import com.ll.exam.jwt_login.app.member.service.MemberService;
 import com.ll.exam.jwt_login.app.security.entity.MemberContext;
 import com.ll.exam.jwt_login.app.security.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -27,7 +24,6 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class JwtAuthorizationFilter extends OncePerRequestFilter {
     private final JwtProvider jwtProvider;
-    private final MemberService memberService;
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String bearerToken = request.getHeader("Authorization");
@@ -37,11 +33,8 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
 
             if (jwtProvider.verify(token)) {
                 Map<String, Object> claims = jwtProvider.getClaims(token);
-                String username = (String) claims.get("username");
-                Member member = memberService.findByUsername(username).orElseThrow(
-                        () -> new UsernameNotFoundException("'%s' Username not found.".formatted(username))
-                );
 
+                Member member = Member.fromJwtClaims(claims);
                 forceAuthentication(member);
             }
         }
